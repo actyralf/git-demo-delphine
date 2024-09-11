01a3f43e3fbe4ff5c8ae5a8bc1cad9501f9d7d03
Ralf Siewert <ralf@actyvy.st>
2024-09-11 10:12:47 +0200

    add task descriptions

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const students = ["Saleh", "Ladan", "Can"];
+
+// Ralf develops a function to randomly select and return a student
+// function name: chooseStudent()
+
+// Anastasiia develops a function that receives a student name and a language and displays a message like this:
+// "Zur Präsentation der Aufgaben wurde Ladan ausgewählt" or
+// "Ladan was selected to present her exercises"
+// function name: displaySelectedStudent(student, language) language: "de" or "en"
